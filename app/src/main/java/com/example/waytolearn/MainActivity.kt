1e87514932f3d813153ec5c8cff15a8d24194f19
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 13:01:02 +0700

    Add the navigation and make the app ready to Run

--- app/src/main/java/com/example/waytolearn/MainActivity.kt
+++ app/src/main/java/com/example/waytolearn/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.waytolearn.ui.WayToLearnNavigaiton
 import com.example.waytolearn.ui.theme.WaytolearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,12 +23,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WayToLearnApp()
                 }
             }
         }
     }
+
+    @Composable
+    fun WayToLearnApp() {
+        WayToLearnNavigaiton()
+    }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
