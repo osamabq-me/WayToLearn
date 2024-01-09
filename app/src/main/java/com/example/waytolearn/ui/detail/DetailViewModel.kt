2d17cf7840f996bd602907df4a97e006bfbd7344
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 12:29:49 +0700

    Creating the Data entry and changing functions

--- app/src/main/java/com/example/waytolearn/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/waytolearn/ui/detail/DetailViewModel.kt
@@ -22,7 +22,143 @@ constructor(
 ) : ViewModel() {
     var state by mutableStateOf(DetailState())
         private set
+
+    init {
+        addListword()
+        getSources()
+        if (wordId != -1) {
+            viewModelScope.launch {
+                repository
+                    .getWordWithSourceAndList(wordId)
+                    .collectLatest {
+                        state = state.copy(
+                            word = it.word.wordName,
+                            source = it.source.sourceName,
+                            meaning = it.word.mean,
+                            category = Utils.category.find { c ->
+                                c.id == it.listingList.id
+                            } ?: Category(),
+                            descript = it.word.descraipe
+                        )
+                    }
+            }
+        }
+    }
+
+
+    init {
+        state = if (wordId != -1){
+            state.copy(isUpdatingword = true)
+        }else{
+            state.copy(isUpdatingword = false)
+        }
+    }
+    val isFieldsNotEmpty: Boolean
+        get() = state.word.isNotEmpty() &&
+                state.source.isNotEmpty() &&
+                state.meaning.isNotEmpty() &&
+                state.descript.isNotEmpty()
+
+    fun onwordChange(newValue: String) {
+        state = state.copy(word = newValue)
+    }
+
+    fun onSourceChange(newValue: String) {
+        state = state.copy(source = newValue)
+    }
+
+    fun onDescrChange(newValue: String) {
+        state = state.copy(descript = newValue)
+    }
+
+    fun onmeanChange(newValue: String) {
+        state = state.copy(meaning = newValue)
+    }
+
+    fun onCategoryChange(newValue: Category) {
+        state = state.copy(category = newValue)
+    }
+
+    fun onScreenDialogDismissed(newValue: Boolean) {
+        state = state.copy(isScreenDialogDismissed = newValue)
+    }
+
+    private fun addListword() {
+        viewModelScope.launch {
+            Utils.category.forEach {
+                repository.insertList(
+                    ListingList(
+                        id = it.id,
+                        name = it.title
+                    )
+                )
+            }
+        }
+    }
+
+    fun addListingword() {
+        viewModelScope.launch {
+            repository.insertWord(
+                Word(
+                    wordName = state.word,
+                    listId = state.category.id,
+                    mean = state.meaning,
+                    descraipe = state.descript,
+                    sourceIdFk = state.sourceList.find {
+                        it.sourceName == state.source
+                    }?.id ?: 0,
+                    isChecked = false
+                )
+            )
+        }
+    }
+
+    fun updateListingword(id: Int) {
+        viewModelScope.launch {
+            repository.insertWord(
+                Word(
+                    wordName = state.word,
+                    listId = state.category.id,
+                    mean = state.meaning,
+                    descraipe = state.descript,
+                    sourceIdFk = state.sourceList.find {
+                        it.sourceName == state.source
+                    }?.id ?: 0,
+                    isChecked = false,
+                    id = id
+                )
+            )
+        }
+    }
+
+    fun addSource() {
+        viewModelScope.launch {
+            repository.insertSource(
+                Source(
+                    sourceName = state.source,
+                    listIdFk = state.category.id
+                )
+            )
+        }
+    }
+
+    fun getSources() {
+        viewModelScope.launch {
+            repository.source.collectLatest {
+                state = state.copy(sourceList = it)
+            }
+        }
+    }
+
 }
+
+@Suppress("UNCHECKED_CAST")
+class DetailViewModelFactor(private  val id: Int): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return DetailViewModel(wordId = id) as T
+    }
+}
+
 data class DetailState(
     val sourceList: List<Source> = emptyList(),
     val word: String = "",
