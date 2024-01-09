f8ba495fad1434de2b7b1511e0ef39a0a71a5aa5
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 11:21:28 +0700

    Creating the Dao for ListDao

--- app/src/main/java/com/example/waytolearn/data/room/Daos.kt
+++ app/src/main/java/com/example/waytolearn/data/room/Daos.kt
@@ -2,12 +2,14 @@ package com.example.waytolearn.data.room
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Embedded
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.waytolearn.data.room.models.Word
 import com.example.waytolearn.data.room.models.Source
+import com.example.waytolearn.data.room.models.ListingList
 import kotlinx.coroutines.flow.Flow
 
 
@@ -48,4 +50,41 @@ interface SourceDao {
     @Query("SELECT * FROM sources WHERE source_id =:sourceId")
     fun getSource(sourceId: Int): Flow<Source>
 }
+
+@Dao
+interface ListDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertListingList(shoppingList: ListingList): Long
+
+    @Query("""
+        SELECT * FROM words AS I INNER JOIN listing_list AS S
+        ON I.listId = S.list_id INNER JOIN sources AS ST
+        ON I.sourceIdFk = ST.source_id
+    """)
+    fun getWordsWithSourceAndList():Flow<List<WordsWithSourceAndList>>
+    @Query("""
+        SELECT * FROM words AS I INNER JOIN listing_list AS S
+        ON I.listId = S.list_id INNER JOIN sources AS ST
+        ON I.sourceIdFk = ST.source_id WHERE S.list_id =:listId
+    """)
+    fun getWordsWithSourceAndListFilteredById(listId:Int)
+            :Flow<List<WordsWithSourceAndList>>
+
+    @Query("""
+        SELECT * FROM words AS I INNER JOIN listing_list AS S
+        ON I.listId = S.list_id INNER JOIN sources AS ST
+        ON I.sourceIdFk = ST.source_id WHERE I.word_id =:wordid
+    """)
+    fun getWordWithSourceAndListFilteredById(wordid: Int)
+            :Flow<WordsWithSourceAndList>
+}
+
+data class WordsWithSourceAndList(
+    @Embedded val word: Word,
+    @Embedded val listingList: ListingList,
+    @Embedded val source: Source,
+)
+
+
+
 
