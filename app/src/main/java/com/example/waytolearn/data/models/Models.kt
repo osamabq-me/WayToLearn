1c7c59a449fe172c12d8e17bbffa6ed2fa882f74
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 11:12:47 +0700

    Creating the Entites and tables for database Word and Source

--- app/src/main/java/com/example/waytolearn/data/models/Models.kt
+++ app/src/main/java/com/example/waytolearn/data/models/Models.kt
@@ -13,5 +13,27 @@ data class ListingList(
     val name: String,
 )
 
+@Entity(tableName = "words")
+data class Word(
+    @ColumnInfo(name = "word_id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val wordName: String,
+    val mean: String,
+    val descraipe: String,
+    val listId: Int,
+    val sourceIdFk: Int,
+    val isChecked: Boolean,
+
+    )
+
+@Entity(tableName = "sources")
+data class Source(
+    @ColumnInfo(name = "source_id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val listIdFk: Int,
+    val sourceName: String,
+)
 
 
