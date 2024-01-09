e8ecc7cfc70337f4f67d16e2560ff8bd0cdf1075
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 14:11:33 +0700

    Update the kotlinCompilerExtensionVersion to work with the dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
     extra.apply {
         set("nav_version", "2.5.3")
@@ -12,5 +13,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 
 }
-
-
