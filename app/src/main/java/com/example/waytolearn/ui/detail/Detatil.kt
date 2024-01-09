2a80fad00b46834e76115d993c1141f491ec9ef2
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 12:46:55 +0700

    Adding the textField for both Source and also the category selection

--- app/src/main/java/com/example/waytolearn/ui/detail/Detatil.kt
+++ app/src/main/java/com/example/waytolearn/ui/detail/Detatil.kt
@@ -1,19 +1,34 @@
 package com.example.waytolearn.ui.detail
 
+import CatagoryWords
 import DetailState
 import DetailViewModel
 import DetailViewModelFactor
 import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.waytolearn.ui.Category
+import com.example.waytolearn.ui.Utils
 import java.util.*
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -73,6 +88,74 @@ private fun DetailEntry(
             modifier = Modifier.fillMaxWidth(),
         )
         Spacer(modifier = Modifier.Companion.size(12.dp))
+
+        Row {
+            TextField(value = state.source,
+                onValueChange = {
+                    if (isNewEnabled) onSourceChange.invoke(it)
+                },
+                modifier = Modifier.weight(1f),
+                label = { Text(text = "Source") },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = null,
+                        modifier = Modifier.clickable {
+                            onDialogDismissed.invoke(!state.isScreenDialogDismissed)
+                        })
+                })
+            if (!state.isScreenDialogDismissed) {
+                Popup(onDismissRequest = {
+                    onDialogDismissed.invoke(!state.isScreenDialogDismissed)
+                }) {
+                    Surface(modifier = Modifier.padding(16.dp)) {
+                        Column {
+                            state.sourceList.forEach {
+                                Text(
+                                    text = it.sourceName,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .clickable {
+                                            onSourceChange.invoke(it.sourceName)
+                                            onDialogDismissed(!state.isScreenDialogDismissed)
+                                        },
+                                )
+
+                            }
+                        }
+                    }
+
+                }
+            }
+            TextButton(onClick = {
+                isNewEnabled = if (isNewEnabled) {
+                    onSaveSource.invoke()
+                    !isNewEnabled
+                } else {
+                    !isNewEnabled
+                }
+            }) {
+                Text(text = if (isNewEnabled) "Save" else "New")
+            }
+        }
+        Spacer(modifier = Modifier.size(12.dp))
+        Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+
+            Spacer(modifier = Modifier.size(12.dp))
+            LazyRow {
+                items(Utils.category) { category: Category ->
+                    CatagoryWords(
+                        iconRes = category.resId,
+                        tittle = category.title,
+                        selcted = category == state.category
+                    ) {
+                        onCategoryChange(category)
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
+                }
+            }
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+
 
     }
 
