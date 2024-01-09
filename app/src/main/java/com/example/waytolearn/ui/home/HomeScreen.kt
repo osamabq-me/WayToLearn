ce0fc82b4d5c4d81e89c4e4dfe24f249afd296d2
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-01-09 11:53:31 +0700

    Creating The TheWordcard to Show the saved data in the database in a card

--- app/src/main/java/com/example/waytolearn/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/waytolearn/ui/home/HomeScreen.kt
@@ -1,28 +1,50 @@
 import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.waytolearn.data.room.WordsWithSourceAndList
+import com.example.waytolearn.ui.Category
+import com.example.waytolearn.ui.Utils
 import com.example.waytolearn.ui.home.HomeViewModel
 
 
@@ -47,6 +69,71 @@ fun HomeScreen(
 }
 
 
+
+
+@Composable
+fun TheWordcard(
+    word: WordsWithSourceAndList,
+    onWordClick: () -> Unit,
+
+    ) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onWordClick.invoke()
+            }
+            .padding(8.dp),
+        border = BorderStroke(2.dp, Color.Blue),
+        colors = CardDefaults.cardColors(containerColor = Color.Cyan)
+    ) {
+
+
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .padding(start = 10.dp)
+                .fillMaxWidth(),
+        ) {
+
+            Text(
+                text = "New word:\n  ${word.word.wordName}",
+                style = MaterialTheme.typography.titleLarge,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = "Word meaning:\n  ${word.word.mean}",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+
+            Text(
+                text = buildAnnotatedString {
+
+                    append("Word description: \n")
+                    withStyle(
+                        SpanStyle(
+                            brush = Brush.linearGradient(
+                                colors = gradientColors
+                            )
+
+
+                        )
+                    ) {
+                        append(word.word.descraipe)
+                    }
+                },
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+
+            Text(text = "Learned From:  ${word.source.sourceName}")
+        }
+    }
+}
 
 
 
